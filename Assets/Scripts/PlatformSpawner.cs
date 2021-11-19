80a25195422321ebb77a41fed7988e3aaebfa1ea
Shahari93 <shaharilani93@gmail.com>
2021-11-19 18:08:05 +0200

    New platform spawn over time
    Procedural Generation:
    Created a new list that holds all the platforms.
    Over time a new platform is spawned at + 7 on the X axis and on Random Y position based on the last platorm in the list.
    Every new platform is added to the list at the last place

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -1,6 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject[] spawnablePlatforms;
+    [SerializeField] List<GameObject> spawnablePlatforms;
+    int lastPlat;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnPlatforms), 3, 5);
+        lastPlat = spawnablePlatforms.Count - 1;
+    }
+
+    private void SpawnPlatforms()
+    {
+        GameObject newPlatGO = Instantiate(ReturnRandomPlatform(), ReturnRandomPlatformPosition(), Quaternion.identity);
+        spawnablePlatforms.Add(newPlatGO);
+        int newIndexForPlat = spawnablePlatforms.Count - 1;
+        lastPlat = newIndexForPlat;
+    }
+
+    private GameObject ReturnRandomPlatform()
+    {
+        int randPlatIndex = Random.Range(0, spawnablePlatforms.Count - 1);
+        GameObject randPlat = spawnablePlatforms[randPlatIndex];
+        return randPlat;
+    }
+
+    private Vector3 ReturnRandomPlatformPosition()
+    {
+        Vector3 newPos = new Vector3(spawnablePlatforms[lastPlat].transform.position.x + 7,
+        Random.Range(spawnablePlatforms[lastPlat].transform.position.y - 2.5f, spawnablePlatforms[lastPlat].transform.position.y + 2.5f),
+        spawnablePlatforms[lastPlat].transform.position.z);
+
+        return newPos;
+    }
 }
